447433b18ab90e52cc32c077cea2f774d4f69012
Łukasz Molenda <lukasz.molenda@CCAEUROPE.LOCAL>
2020-08-19 21:49:22 +0200

    new Api entity: Tasks and not Api entity:ProjectUserRel

---
+++ src/Entity/Tasks.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\TasksRepository")
+ */
+class Tasks
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=10)
+     */
+    private $code;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     * @Groups({"hours:read"})
+     */
+    private $description;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Projects")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $project;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function setCode(string $code): self
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getProject(): ?Projects
+    {
+        return $this->project;
+    }
+
+    public function setProject(?Projects $project): self
+    {
+        $this->project = $project;
+
+        return $this;
+    }
+}
