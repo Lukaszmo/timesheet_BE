4728fa4364c0e0a8577deedc7e9faa80df078aa4
Łukasz Molenda <lukasz.molenda@CCAEUROPE.LOCAL>
2020-12-12 22:05:53 +0100

    added new routes

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -10,6 +10,10 @@ class DefaultController extends AbstractController
 {
     /**
      * @Route("/{reactRouting}", name="home", defaults={"reactRouting": null})
+     * @Route("/czas-pracy/{reactRouting}", name="czas-pracy")
+     * @Route("/urlopy/{reactRouting}", name="urlopy")
+     * @Route("/ustawienia/{reactRouting}", name="ustawienia")
+     * @Route("/raporty/{reactRouting}", name="raporty")
      * @Route("/panel-admina/{reactRouting}", name="panel-admina")
      */
   /*  public function index()
