fe22c70d6e02d13eb9d2866a5495bbb595cb2c1d
Łukasz Molenda <lukasz.molenda@CCAEUROPE.LOCAL>
2021-01-07 15:05:35 +0100

    new property of Projects entity

--- src/Entity/Projects.php
+++ src/Entity/Projects.php
@@ -12,7 +12,7 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 /**
  * @ApiResource()
  * @ORM\Entity(repositoryClass="App\Repository\ProjectsRepository")
- * 
+ *
  * @UniqueEntity("code")
  */
 class Projects
@@ -34,7 +34,7 @@ class Projects
     /**
      * @ORM\Column(type="string", length=50)
      * @Groups({"hours:read"})
-     * 
+     *
      */
     private $description;
 
@@ -44,46 +44,63 @@ class Projects
      */
     private $client;
 
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $active;
+
 
     public function getId(): ?int
     {
         return $this->id;
     }
-
+    
     public function getCode(): ?string
     {
         return $this->code;
     }
-
+    
     public function setCode(string $code): self
     {
         $this->code = $code;
-
+        
         return $this;
     }
-
+    
     public function getDescription(): ?string
     {
         return $this->description;
     }
-
+    
     public function setDescription(string $description): self
     {
         $this->description = $description;
-
+        
         return $this;
     }
-
+    
     public function getClient(): ?Clients
     {
         return $this->client;
     }
-
+    
     public function setClient(?Clients $client): self
     {
         $this->client = $client;
-
+        
         return $this;
     }
-
+    
+    public function getActive(): ?bool
+    {
+        return $this->active;
+    }
+    
+    public function setActive(bool $active): self
+    {
+        $this->active = $active;
+        
+        return $this;
+    }
+    
 }
