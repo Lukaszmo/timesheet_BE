2a807b5c2d02c1efb929c6fd34d60b08d7339ec2
Łukasz Molenda <lukasz.molenda@CCAEUROPE.LOCAL>
2021-03-28 14:51:53 +0200

    move addTaskToProject function to project service

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -5,12 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use App\Entity\ProjectTaskRel;
-use App\Entity\Projects;
-use App\Entity\Tasks;
-use Symfony\Component\HttpKernel\Exception\HttpException;
+
 
 use App\Service\Project;
 
@@ -38,41 +34,20 @@ class ProjectController extends AbstractController{
         return $response;
     }
     
+    
     /**
      * @route("/project/task_add", name="project_task_add", methods={"POST"})
      */
     public function addTaskToProject(Request $request){
         
-        $postData = [];
+        $postData = Array();
         
         if($content = $request->getContent()){
             
             $postData = json_decode($content,true);
         }
         
-        $em = $this->getDoctrine()->getManager();
-      
-        $em->beginTransaction();
-        
-        try{
-      
-            foreach ($postData as $value) {
-              
-                $projectTask= new ProjectTaskRel();
-                $project = $this->getDoctrine()->getRepository(Projects::class)->findOneBy(array('id' => $value['project']));
-                $task = $this->getDoctrine()->getRepository(Tasks::class)->findOneBy(array('id' => $value['task']));
-                $projectTask->setProject($project);
-                $projectTask->setTask($task);
-                $projectTask->setActive(true);
-                $em->persist($projectTask);
-                $em->flush();
-            } 
-            $em->commit();
-            
-        } catch (\Exception $e) {
-            $em->rollBack();
-            throw $e;
-        }
+        $this->project->addTaskToProject($postData);
         
         $response = new JsonResponse('Records created',201);
         
--- src/Service/Project.php
+++ src/Service/Project.php
@@ -3,15 +3,23 @@
 namespace App\Service;
 
 use App\Repository\ProjectUserRelRepository;
+use Doctrine\ORM\EntityManagerInterface;
+
+use App\Entity\ProjectTaskRel;
+use App\Entity\Projects;
+use App\Entity\Tasks;
 
 
 class Project {
     
     private $projectUserRep;
     
-    public function __construct(ProjectUserRelRepository $rep){
+    private $em;
+    
+    public function __construct(ProjectUserRelRepository $rep, EntityManagerInterface $em){
         
         $this->projectUserRep = $rep;
+        $this->em = $em;
     }
     
     public function getProjects(int $id): ? array {
@@ -40,5 +48,32 @@ class Project {
         } 
         
         return $list;
+    }
+    
+    public function addTaskToProject(array $ProjectTaskArray){
+        
+        $this->em->beginTransaction();
+        
+        try{
+            
+            foreach ($ProjectTaskArray as $value) {
+                
+                $projectTask = new ProjectTaskRel();
+                $project = $this->em->getRepository(Projects::class)->findOneBy(array('id' => $value['project']));
+                $task = $this->em->getRepository(Tasks::class)->findOneBy(array('id' => $value['task']));
+                $projectTask->setProject($project);
+                $projectTask->setTask($task);
+                $projectTask->setActive(true);
+                $this->em->persist($projectTask);
+                $this->em->flush();
+            }
+        
+            $this->em->commit(); 
+            
+        } catch (\Exception $e) {
+            
+            $this->em->rollBack();
+            throw $e;
+        }
     }
 }
